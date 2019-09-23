491859d143433fd5bbc94d291851f05c7c5ef0f1
Forest Handford <forest.handford@affectiva.com>
2019-09-23 18:15:19 -0400

    Added Find Duplicates.

---
+++ find_duplicates.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+# Written by Forest J. Handford (She/Her)
+
+# This is a Ruby implementation of the ideal solution for a common interview
+# question.
+class FindDupes
+  # Answer for current array (22, 88, 134) are duplicates
+  NUMBERS = [1, 88, 18, 88, 22, 134, 95, 134, 5, 8, 88, 22, 59, 134, 3, 42, 133, 67, 163].freeze
+  attr_accessor :count
+  
+  def build_count
+    @count = []
+    NUMBERS.each do |num|
+      if @count[num].nil?
+        @count[num] = 1
+      else
+        @count[num] += 1
+      end
+    end
+  end
+
+  def list_dupes
+    build_count
+    # This is expensive!
+    @count.each_with_index do |val, index|
+      if val.nil?
+      elsif @count[index] > 1
+        puts 'There are ' + val.to_s + ' of number ' + index.to_s + ' in the list!'
+      end
+    end
+  end
+end
+
+FindDupes.new.list_dupes if $PROGRAM_NAME == __FILE__
