985a70d7147e57d62d649607c49375f387c87aed
ForestJay <forest.handford@gmail.com>
2019-09-29 14:28:34 -0400

    Added tests for SumSrc

---
+++ test_find_sum_src.rb
@@ -0,0 +1,12 @@
+# Written by Forest J. Handford (She/Her)
+
+require_relative 'sum_src'
+require 'minitest/autorun'
+
+# Class for testing
+class TestSumSrc < Minitest::Test
+  def test_common_cases
+    assert_equal [1, 9], SumSrc.new([1, 2, 3, 9], 8).find
+    assert_equal [4, 4], SumSrc.new([1, 2, 4, 4], 8).find
+  end
+end
