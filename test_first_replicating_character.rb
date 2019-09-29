4b8d5c90ba9a63d91a32c61a21611e6063399e0a
ForestJay <forest.handford@gmail.com>
2019-09-29 13:29:38 -0400

    Fixed silly typo that IDE and Rubocop don't notice

--- test_first_replicating_character.rb
+++ test_first_replicating_character.rb
@@ -6,8 +6,8 @@ require 'minitest/autorun'
 # Class for testing
 class TestFindFirstReplicatingCharacter < Minitest::Test
   def test_common_cases
-    assert_equals 'A', FirstReplicatingCharacter.new('ABCA').find
-    assert_equals 'B', FirstReplicatingCharacter.new('BCABA').find
-    assert_equals nil, FirstReplicatingCharacter.new('ABCA').find
+    assert_equal 'A', FirstReplicatingCharacter.new('ABCA').find
+    assert_equal 'B', FirstReplicatingCharacter.new('BCABA').find
+    assert_equal nil, FirstReplicatingCharacter.new('ABCA').find
   end
 end
