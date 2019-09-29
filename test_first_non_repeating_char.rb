b20b11a89c9c24199dcbd61d96e52d7dae7e0dc6
ForestJay <forest.handford@gmail.com>
2019-09-29 13:56:17 -0400

    Adding tests for non repeating chars

---
+++ test_first_non_repeating_char.rb
@@ -0,0 +1,15 @@
+# Written by Forest J. Handford (She/Her)
+
+require_relative 'first_unrepeated_character'
+require 'minitest/autorun'
+
+# Class for testing
+class TestFindFirstUnrepeatedCharacter < Minitest::Test
+  def test_common_cases
+    assert_equal 'B', FirstUnrepeatedCharacter.new('ABCA').find
+    assert_equal 'C', FirstUnrepeatedCharacter.new('BCABA').find
+    assert_nil FirstUnrepeatedCharacter.new('ABCABC').find
+    assert_equal 'B', FirstUnrepeatedCharacter.new('DBCABA').find
+    assert_equal 'A', FirstUnrepeatedCharacter.new('ABC').find
+  end
+end
