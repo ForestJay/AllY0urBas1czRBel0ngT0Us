47d9676575f72fe3eca502fa1e0f7c3d5c48c399
ForestJay <forest.handford@gmail.com>
2019-09-29 13:35:37 -0400

    Added FirstReplicatingCharacter class

---
+++ first_replicating_character.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+# Written by Forest J. Handford (She/Her)
+
+# This is a Ruby implementation of the ideal solution for a common interview
+# question.
+class FirstReplicatingCharacter
+  def initialize(string)
+    @string = string
+  end
+
+  # Return the first repeating character else nil
+  def find
+    total = {}
+    @string.split('').each do |char|
+      if total[char].nil?
+        total[char] = 1
+      else
+        return char
+      end
+    end
+    nil
+  end
+end
