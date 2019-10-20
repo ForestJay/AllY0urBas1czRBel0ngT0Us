1b62743f12ce20faabba588cb3698eceaceebca8
Forest Handford <forest.handford@affectiva.com>
2019-10-20 14:10:23 -0400

    More assertions for Hamster.

--- test_hamster.rb
+++ test_hamster.rb
@@ -22,4 +22,18 @@ class TestHamster < Minitest::Test
               Hamster.new(gen[2][12], gen[2][13]), Hamster.new(gen[2][14], gen[2][14])]
     assert gen[3][0].related_to(gen[2][7])
   end
+  
+  def test_relations
+    gen = []
+    gen[1] = [Hamster.new(false, false), Hamster.new(false, false),
+              Hamster.new(false, false), Hamster.new(false, false),
+              Hamster.new(false, false), Hamster.new(false, false),
+              Hamster.new(false, false), Hamster.new(false, false)]
+    gen[2] = [Hamster.new(gen[1][0], gen[1][1]), Hamster.new(gen[1][2], gen[1][3]), 
+              Hamster.new(gen[1][4], gen[1][5]), Hamster.new(gen[1][6], gen[1][7]), 
+              Hamster.new(gen[1][4], gen[1][5]), Hamster.new(gen[1][4], gen[1][5])]
+    assert ! gen[2][0].related_to(gen[2][1])
+    assert ! gen[2][2].related_to(gen[2][3])
+    assert gen[2][4].related_to(gen[2][5])
+  end
 end
