dd6f90376f1153a6b2e50837d0574d88ea5226fa
Forest Handford <fhandfor@broadinstitute.org>
2021-07-29 10:39:40 -0400

    Added ability to import JSON format

--- company.rb
+++ company.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
+
 #
 # An example of loading a YAML file
 
 require 'yaml'
+require 'json'
 
 # Company class
 class Company
@@ -12,14 +14,24 @@ class Company
   def import(file_path)
     return unless File.exist?(file_path)
 
-    File.exist?(file_path)
-    yaml_data = YAML.load_file(file_path)
-    @name = yaml_data['name']
-    @ceo = yaml_data['ceo']
-    @stock_isin = yaml_data['stock_isin']
-    @ceo_annual_sold_shares = yaml_data['ceo_annual_sold_shares']
-    @ceo_shares = yaml_data['ceo_shares']
-    @ceo_annual_bonus = yaml_data['ceo_annual_bonus']
-    @ceo_annual_salary = yaml_data['ceo_annual_salary']
+    file_data = get_file_data(file_path)
+    @name = file_data['name']
+    @ceo = file_data['ceo']
+    @stock_isin = file_data['stock_isin']
+    @ceo_annual_sold_shares = file_data['ceo_annual_sold_shares']
+    @ceo_shares = file_data['ceo_shares']
+    @ceo_annual_bonus = file_data['ceo_annual_bonus']
+    @ceo_annual_salary = file_data['ceo_annual_salary']
+  end
+
+  private
+
+  def get_file_data(file_path)
+    case File.extname(file_path)
+    when '.yml'
+      YAML.load_file(file_path)
+    when '.json'
+      JSON.load_file(file_path)
+    end
   end
 end
--- test_company.rb
+++ test_company.rb
@@ -7,10 +7,19 @@ require 'minitest/autorun'
 
 # Class for testing
 class TestCompany < Minitest::Test
-  def test_common_cases
+  def test_yaml
     facebook = Company.new
     facebook.import('facebook.yml')
+    value_assertions(facebook)
+  end
 
+  def test_json
+    facebook = Company.new
+    facebook.import('facebook.json')
+    value_assertions(facebook)
+  end
+
+  def value_assertions(facebook)
     assert_equal 'Mark Zuckerburg', facebook.ceo
     assert_equal 'Facebook', facebook.name
   end
